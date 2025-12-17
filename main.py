2692d555b250032af009ea7befbb4c79368347b9
Danilo Drobnjak <drobnjakdm@gmail.com>
2025-12-17 17:10:15 +0100

    add settings json for actuators and sensors

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+try:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+except:
+    pass
+
 if __name__ == "__main__":
     pass
---
+++ utils.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO
+
+# GPIO pull-up/down mapping
+PULL_MAP = {
+    "up": GPIO.PUD_UP,
+    "down": GPIO.PUD_DOWN
+}
