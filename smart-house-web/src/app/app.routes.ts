2fc3cf014fd9bcdadd8bc5fccdafd77e95b2d6d8
Danilo Drobnjak <drobnjakdm@gmail.com>
2026-02-22 20:08:18 +0100

    added camera page

--- smart-house-web/src/app/app.routes.ts
+++ smart-house-web/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from '../pages/home/home.component';
+import {CameraComponent} from '../pages/camera/camera.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'camera',
+    component: CameraComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+];
