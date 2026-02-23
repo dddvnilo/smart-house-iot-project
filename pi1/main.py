c7a5eb910dc4b4d4e7a25383fc8ed5f1ba037e90
urosh2003 <radukic.uros@gmail.com>
2026-02-23 19:53:18 +0100

    Implemented unlocked door alarm

--- pi1/main.py
+++ pi1/main.py
@@ -70,20 +70,20 @@ if __name__ == "__main__":
         
         # ucitavanje podesavanja i pokretanje komponenti
         
-        #ds1_settings = settings['DS1']
-        #ds1 = run_ds1(ds1_settings, threads, stop_event)
+        ds1_settings = settings['DS1']
+        ds1 = run_ds1(ds1_settings, threads, stop_event)
 
         dl_settings = settings['DL']
         dl = run_dl(dl_settings, threads, stop_event)
 
-        dus1_settings = settings['DUS1']
-        dus1 = run_dus1(dus1_settings, threads, stop_event)
+        #dus1_settings = settings['DUS1']
+        #dus1 = run_dus1(dus1_settings, threads, stop_event)
 
         db_settings = settings['DB']
         db = run_db(db_settings, threads, stop_event)
 
-        dpir1_settings = settings['DPIR1']
-        dpir1 = run_dpir1(dpir1_settings, threads, stop_event)
+        #dpir1_settings = settings['DPIR1']
+        #dpir1 = run_dpir1(dpir1_settings, threads, stop_event)
 
         #dms_settings = settings['DMS']
         #dms = run_dms(dms_settings, threads, stop_event)
